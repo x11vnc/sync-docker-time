251031e1a4c80cd10b5dad60c9f0f33121777d18
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-15 14:06:36 -0400

    Updated uninstall scripts

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 if [ -f ~/Library/LaunchAgents/io.github.sync-docker-time.plist ];
 then
   launchctl list | grep -q io.github.sync-docker-time &&
-    launchctl remove io.github.sync-docker-time
+    launchctl unload ~/Library/LaunchAgents/io.github.sync-docker-time.plist
   killall sleepwatcher-sdt
   rm -f ~/Library/LaunchAgents/io.github.sync-docker-time.plist
   rm -rf ~/Library/SyncDockerTime/
