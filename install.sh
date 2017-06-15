d5857eab9191f2732bea5f1ccefa1fda51726aeb
Xiangmin Jiao <xmjiao@gmail.com>
2017-06-15 13:38:57 -0400

    Renamed bin/sleepwatcher -> bin/sleepwatcher-sdt

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 if [ ! -f ~/Library/SyncDockerTime/io.github.sync-docker-time.plist ];
 then
   mkdir -p ~/Library/SyncDockerTime/
-  curl https://github.com/x11vnc/sync-docker-time/archive/master.zip | \
-      bsdtar xf -C ~/Library/SyncDockerTime/ --strip 1
+  curl -L -s https://github.com/x11vnc/sync-docker-time/archive/master.zip | \
+      bsdtar xf - -C ~/Library/SyncDockerTime/ --strip-components 1
   rm -rf ~/Library/SyncDockerTime/sleepwatcher_2.2
   chmod a+x ~/Library/SyncDockerTime/bin/*
   ln -s -f ~/Library/SyncDockerTime/io.github.sync-docker-time.plist \
--- uninstall.sh
+++ uninstall.sh
@@ -2,8 +2,9 @@
 
 if [ -f ~/Library/LaunchAgents/io.github.sync-docker-time.plist ];
 then
-  launchctl list | grep -q io.github.sync-docker-time.plist &&
-    launchctl unload ~/Library/LaunchAgents/io.github.sync-docker-time.plist
+  launchctl list | grep -q io.github.sync-docker-time &&
+    launchctl remove io.github.sync-docker-time
+  killall sleepwatcher-sdt
   rm -f ~/Library/LaunchAgents/io.github.sync-docker-time.plist
   rm -rf ~/Library/SyncDockerTime/
 fi
